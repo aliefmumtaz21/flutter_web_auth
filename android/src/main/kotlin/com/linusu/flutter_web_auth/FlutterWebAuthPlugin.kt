b76feea217d52bf420eca5da55ff12efacd3e815
Alief Mumtaz <86474503+aliefmumtaz@users.noreply.github.com>
2022-12-05 16:57:14 +0800

    Revert "Update FlutterWebAuthPlugin.kt"
    This reverts commit 630d09a32d4295505a2c79184d75a09d4588191a.

--- android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
+++ android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
@@ -2,8 +2,6 @@ package com.linusu.flutter_web_auth
 
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.pm.ResolveInfo
 import android.net.Uri
 
 import androidx.browser.customtabs.CustomTabsIntent
@@ -25,6 +23,7 @@ class FlutterWebAuthPlugin(private var context: Context? = null, private var cha
         val plugin = FlutterWebAuthPlugin()
         plugin.initInstance(registrar.messenger(), registrar.context())
     }
+
   }
 
   fun initInstance(messenger: BinaryMessenger, context: Context) {
@@ -42,28 +41,12 @@ class FlutterWebAuthPlugin(private var context: Context? = null, private var cha
       channel = null
   }
 
-  private fun getCustomTabsPackages(context: Context): List<ResolveInfo> {
-    val pm: PackageManager = context.packageManager
-    // Get default VIEW intent handler.
-    val activityIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.example.com"))
-    // Get all apps that can handle VIEW intents.
-    val resolvedActivityList: List<ResolveInfo> = pm.queryIntentActivities(activityIntent, 0)
-    return resolvedActivityList.filter {
-        val serviceIntent = Intent()
-        serviceIntent.action = "android.support.customtabs.action.CustomTabsService"
-        serviceIntent.setPackage("com.android.chrome")
-        // Check if this package also resolves the Custom Tabs service.
-        pm.resolveService(serviceIntent, 0) != null
-    }
-}
-
   override fun onMethodCall(call: MethodCall, resultCallback: Result) {
     when (call.method) {
         "authenticate" -> {
           val url = Uri.parse(call.argument("url"))
           val callbackUrlScheme = call.argument<String>("callbackUrlScheme")!!
           val preferEphemeral = call.argument<Boolean>("preferEphemeral")!!
-          val preferWeb = call.argument<Boolean>("preferWeb")!!
 
           callbacks[callbackUrlScheme] = resultCallback
 
@@ -74,12 +57,8 @@ class FlutterWebAuthPlugin(private var context: Context? = null, private var cha
           if (preferEphemeral) {
               intent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
           }
-
-          if (preferWeb) {
-              serviceIntent.setPackage("com.android.chrome")
-          }
-
           intent.intent.putExtra("android.support.customtabs.extra.KEEP_ALIVE", keepAliveIntent)
+
           intent.launchUrl(context!!, url)
         }
         "cleanUpDanglingCalls" -> {
@@ -93,4 +72,3 @@ class FlutterWebAuthPlugin(private var context: Context? = null, private var cha
     }
   }
 }
-
